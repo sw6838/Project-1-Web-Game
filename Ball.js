51955987019efd29da3bac0ae1a7a9f549cb195f
Shabnam Waseem <shabnamwaseem@Shabnams-MacBook-Air.local>
2022-02-20 13:13:04 -0800

    Updated

--- Ball.js
+++ Ball.js
@@ -1,7 +1,22 @@
 
+/*Set an export default for our class Ball*/
 export default class Ball{
     constructor(ballElem) {
+        // ballElem is the ball on the screen
     this.ballElem = ballElem  
     }
+
+    /* helper functions using balls direction and velocity 
+    to help us calculate if it is bouncing off of wall or bouncing off of paddles*/
+     get x(){
+         return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+     }
+     set x (value){
+         this.ballElem.style.setProperty("--x", value)
+     }
+    // update function takes in delta script.js
+    update(delta) {
+        this.x = 5
+
+    }
 }
-    
--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+// Import ball from default export Ball.js
 import Ball from "./Ball.js"
-const ball = new Ball(document.getElementbyId("ball"))
+
+// Ball is a new ball with html Id 'ball'
+//selecting an html element and creating a new class for ball
+const ball = new Ball(document.getElementById("ball"))
+
+//Variable lastTime
 let lastTime
+
+// How much time is passed since the start of the program
 function update(time) {
+    if (lastTime != null){
+        const delta = time - lastTime
+        ball.update(delta)
+    }
+        
+        lastTime = time
+    
+  //Every frame is going to call update loop function
    window.requestAnimationFrame(update)
 }
+
+window.requestAnimationFrame(update)
