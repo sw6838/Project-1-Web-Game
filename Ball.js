a1460cc6582fb3102079ab08d9d7b9a186fdbe7c
Shabnam Waseem <shabnamwaseem@Shabnams-MacBook-Air.local>
2022-02-20 13:46:47 -0800

    Updated

--- Ball.js
+++ Ball.js
@@ -3,10 +3,12 @@
 export default class Ball{
     constructor(ballElem) {
         // ballElem is the ball on the screen
-    this.ballElem = ballElem  
+    this.ballElem = ballElem 
+    // To reset balls and paddles at 50% in the middle of the page
+    this.reset() 
     }
 
-    /* helper functions using balls direction and velocity 
+    /* Get and Set helper functions using balls direction and velocity 
     to help us calculate if it is bouncing off of wall or bouncing off of paddles*/
      get x(){
          return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
@@ -21,6 +23,21 @@ export default class Ball{
     set y (value){
         this.ballElem.style.setProperty("--y", value)
     }
+
+    // To reset balls and paddles at 50% in the middle of the page
+    reset(){
+        this.x = 50
+        this.y = 50
+        this.direction = { x:0 }
+        while(Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9){
+            // create random value between 0-2 pi to determine our direction
+                const heading = randomNumberBetween(0, 2 * Math.PI)
+                // take that direction and convert it into x and y position
+                this.direction = {x: Math.cos(heading), y: Math.sin(heading)}
+        }
+        console.log(this.direction)
+    }
+
     // update function takes in delta script.js
     update(delta) {
         this.x = 5
@@ -28,3 +45,7 @@ export default class Ball{
 
     }
 }
+    function randomNumberBetween(min, max) {
+        return Math.random() * (max-min) + min
+        
+    }
